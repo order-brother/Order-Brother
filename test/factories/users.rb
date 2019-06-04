13aa7b7992edd85c983060b0f56958fcfdcdb244
Nine Bro <shinjisrg@gmail.com>
2019-06-04 20:11:34 +0800

    Add User model.

---
+++ test/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    title { "MyString" }
+    name { "MyString" }
+    phone { "MyString" }
+    password { "MyString" }
+    email { "MyString" }
+  end
+end
