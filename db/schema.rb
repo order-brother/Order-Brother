297613845794159fd6e35945c2e9b9a6b13877ad
Nine Bro <shinjisrg@gmail.com>
2019-06-05 21:04:19 +0800

    Create store table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_124026) do
+ActiveRecord::Schema.define(version: 2019_06_05_130057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.string "latitude"
+    t.string "tel"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "title"
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_06_05_124026) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "stores", "users"
 end
