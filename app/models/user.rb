9f5f653cad3fb9e506513afe91fa1000b6f2a328
Nine Bro <shinjisrg@gmail.com>
2019-06-08 00:07:18 +0800

    Allows user login with phone number and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,29 @@
 class User < ApplicationRecord
+  # Tell devise to use :login in the authentication_keys
+  attr_writer :login
+
+  validates :phone, presence: true
+  validates_format_of :phone, with: /^\d{10}$/, multiline: true
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
   has_many :stores
   has_many :transactions
+
+  def login
+    @login || self.phone || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["phone = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:phone) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
