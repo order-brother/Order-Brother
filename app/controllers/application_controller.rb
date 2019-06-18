7378332187d76d1bfe9a902c74f0e056275d713b
Steven C.(Nine Bro.) <stevencch99@gmail.com>
2019-06-18 17:02:31 +0800

    Move transaction state-change buttons to orders_card--body.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   
