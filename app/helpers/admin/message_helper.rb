6ae75f815a61a3ad5340f154791bb2a2ca20fc51
Steven C.(Nine Bro.) <stevencch99@gmail.com>
2019-06-20 17:35:04 +0800

    fix most of elements on landing page.

--- app/helpers/admin/message_helper.rb
+++ app/helpers/admin/message_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Admin
   module MessageHelper
     def flash_message(msg_type)
@@ -7,12 +8,12 @@ module Admin
     def error_block(store)
       # msg = ['<p class="error_messages">TEST message_helper</p>']
       msg = []
-      if store.errors.any? 
+      if store.errors.any?
         msg << '<ul class="error_messages">'
         store.errors.full_messages.each do |message|
-          msg << "<li>#{message}</li>"
+          msg << "<li class='alert alert-danger'>#{message}</li>"
         end
-        msg << "</ul>"
+        msg << '</ul>'
       end
       sanitize(msg.join(''))
     end
