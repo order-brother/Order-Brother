46b0ab97a732cabb63590dd9ef710e7a1ffc3c12
Steven C <stevencch99@gmail.com>
2019-07-04 16:25:02 +0800

    Fix MainImgUploader to upload either in local/public dir or to AWS. Fix landing page of store cards' height.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  # if Rails.env.staging? || Rails.env.production?
+  if Rails.env.staging? || Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:               'AWS',
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
       region: ENV['S3_BUCKET_REGION']
     }
     config.fog_directory = ENV['S3_BUCKET_NAME']
-  # else
-  #   config.storage = :file
-  #   config.enable_processing = Rails.env.development?
-  # end
+  else
+    config.storage = :file
+    config.enable_processing = Rails.env.development?
+  end
 end
