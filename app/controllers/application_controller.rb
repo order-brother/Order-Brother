520ad226efabf1844575e616bcba33d240f0e1a3
Steven C <stevencch99@gmail.com>
2019-07-05 19:41:06 +0800

    Change Time to Time.zone, refactor duplicated method to application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def default_pick_up_time(current_time = Time.zone.now + 30.minutes)
+    current_time = Time.zone.parse(current_time)
+    current_time ||= Time.zone.now + 30.minutes
+    current_time.strftime("%Y-%m-%dT%H:%M")
+  end
 end
